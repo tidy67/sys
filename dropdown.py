067bebc19c4d53b07c8d51356a3aa877e0015a61
tidy67 <rtopune4518@gmail.com>
2025-11-19 09:26:13 +0530

    Add files via upload

--- dropdown.py
+++ dropdown.py
@@ -3,19 +3,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
-driver=webdriver.Chrome()
+driver = webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/dropdown")
-mydropdown=driver.find_element(By.ID,"dropdown")
-dropdown=Select(mydropdown)
 
+# locate dropdown
+mydropdown = driver.find_element(By.ID, "dropdown")
+dropdown = Select(mydropdown)
+
+# select by visible text
 dropdown.select_by_visible_text("Option 1")
-time.sleep(5)
+time.sleep(2)
+print(f"Selected option: {dropdown.first_selected_option.text}")
 
-selected_option=dropdown.first_selected_option
-print(f"Selected option: {selected_option.text}")
-dropdown.select_by_value("2")  #Select Option 2 by value
-time.sleep(5)
-dropdown.select_by_index(1)  #index starts from 0, here 0th position is default option
-time.sleep()
-dropdown.deselect_by_visible_text("Option 1")  #error?
-dropdown.deselect_all() #error?
+# select by value
+dropdown.select_by_value("2")
+time.sleep(2)
+print(f"Selected option: {dropdown.first_selected_option.text}")
+
+# select by index (2 → Option 2)
+dropdown.select_by_index(2)
+time.sleep(2)
+print(f"Selected option: {dropdown.first_selected_option.text}")
+
+
--- frame.py
+++ frame.py
@@ -1,25 +1,24 @@
 from selenium import webdriver
-from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
 driver.get("https://the-internet.herokuapp.com/iframe")
-time.sleep(10)
 
+# Switch into iframe
 driver.switch_to.frame("mce_0_ifr")
-time.sleep(10)
 print("Switched to iframe")
 
-
 iframe_body = driver.find_element(By.TAG_NAME, "body")
-time.sleep(2)
 iframe_body.send_keys("India")
-time.sleep(10)
+
+time.sleep(5)
+
 driver.switch_to.default_content()
-time.sleep(2)
 
 main_header = driver.find_element(By.TAG_NAME, "h3")
-print("Main Document Header", main_header.text)
+print("Main Document Header: ", main_header.text)
+
+time.sleep(5)
 
 driver.quit()
